ec07a7617f7e72bc93a57f9fd2c82a87f0612c2c
Morten Mc <Morten.M.Cramer@infomedia.dk>
2020-03-02 17:10:10 +0100

    Making buttons in the TopBar

--- src/App.js
+++ src/App.js
@@ -5,41 +5,33 @@ import FrontPage from '../src/FrontPage/FrontPage'
 import EducationPage from '../src/EducationPage/EducationPage'
 import ExperiencePage from '../src/ExperiencePage/ExperiencePage'
 import "./App.css"
+import './TopBar.css'
+import MMCLogoBlack from '../Pictures/MMCLogoSort.png'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
   render() {
     return (
         <div>
-          <TopBar/>
+          <div className='TopBarLayer'>
+            <div className='TopBarView'>
+                <div className='TopBarContent'>
+                    <div className='LogoContainer'>
+                        <img className='LogoContent' src={MMCLogoBlack} alt={MMCLogoBlack}/>
+                    </div>
+                    <div className='CVContainer'>
+                        <div className='CVContent'>
+                            Curriculum Vitae
+                        </div>
+                    </div>
+                    <div>
+                      <button>Forside</button>
+                      <button>Uddannelser</button>
+                      <button>Erfaring</button>
+                    </div>
+                </div> 
+            </div>
+          </div>
           <div>
             <FrontPage/>
             <EducationPage/>
