1b97f9266f334ba1a022a7a5f99be5fac93067df
Morten Mc <Morten.M.Cramer@infomedia.dk>
2020-03-02 14:13:53 +0100

    CSS styles for TopBar

--- src/TopBar/TopBar.js
+++ src/TopBar/TopBar.js
@@ -4,15 +4,15 @@ import './TopBar.css'
 class TopBar extends React.Component {
     render() {
         return (
-        <div style={{display: 'flex', marginTop: '10px', justifyContent: 'center'}}>
+        <div className='TopBarLayer'>
             <div className='TopBarView'>
                 <div className='TopBarContent'>
-                    <div style={{display: 'flex', alignItems: 'center', marginLeft: '50px'}}>
-                        <div style={{height: '50px', margin: '10px'}}>Logo</div>
+                    <div className='LogoContainer'>
+                        <div className='LogoContent'>Logo</div>
                         {/* <img style={{height: '50px', margin: '10px'}} src={MMCLogoBlack} alt={MMCLogoBlack}/> */}
                     </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <div style={{fontSize: '25px', color: 'black', fontWeight: 600}}>
+                    <div className='CVContainer'>
+                        <div className='CVContent'>
                             Curriculum Vitae
                         </div>
                     </div>
